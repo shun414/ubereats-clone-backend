0407e1840b33cfe2aad66ad1d7ab51cf2bf55dc6
shun414 <shunshira14@gmail.com>
2021-07-18 14:19:11 +0900

    add restaurant spec

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe 'Restaurant', type: :request do
   let!(:restaurant) { create(:restaurant) }
 
   describe 'GET /restaurants' do
-    it 'returns restaurants' do
+    it 'returns 200' do
       get '/api/v1/restaurants'
       expect(response).to have_http_status(:success)
+    end
+    it 'returns restaurants' do
+      get '/api/v1/restaurants'
+      pp response.body
+      pp restaurant.fee
+      expect(response.body).to include(restaurant.name)
+      expect(response.body).to include(restaurant.fee.to_s)
+      expect(response.body).to include(restaurant.time_required.to_s)
     end
   end
 end
